254ee344ce94b6454d6e3e68bfd2eb1fe250f2d0
IrishAdo <spambot.bmg@googlemail.com>
2011-10-02 00:23:45 +0100

    Added comments to start file

--- Application/start.js
+++ Application/start.js
@@ -1,7 +1,12 @@
 /**
  * This Script starts the application server
+ * ------------------------------------------
+ * @author irishado@hotmail.com
+ * @copyright Creative Commons
  */
 
+// create a object from our server.js file
 var server = require("./Modules/server");
 
+// execute the start function and pass a basic parameter throught to override the default settings.
 server.start({port:801});
